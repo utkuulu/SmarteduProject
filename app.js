9e8e9d6585834f4a9f19f79463896051a5f86b82
Utku Uluışık <utkub175@gmail.com>
2023-05-17 14:02:56 +0300

    Lesson 02

--- app.js
+++ app.js
@@ -2,9 +2,27 @@ const express = require('express');
 
 const app = express();
 
+// Template Engine
+app.set("view engine", "ejs");
+
+// MiddleWares
+app.use(express.static("public"));
+
+
+// ROUTES 
+
 app.get('/', (req, res) => {
-  res.status(200).send('INDEX SAYFASI');
+  res.status(200).render('index', {
+    page_name: "index"
+  });
 });
+
+app.get('/about', (req, res) => {
+    res.status(200).render('about', {
+        page_name: "about"
+      });
+  });
+  
 
 const port = 3000;
 app.listen(port, () => {
